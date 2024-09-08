dae0682f3709c9c0a03ccd27de8e2f823c2432ff
Toms <Toms@WIN-PRN7IADR449>
2024-09-08 12:01:28 +0700

    Add project files.

---
+++ hocConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = new int[10];
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write("a[" + i + "]: ");
+            arr[i] = int.Parse(Console.ReadLine());
+            Console.Write("\n");
+        }
+
+        int b = 0;
+        foreach (int x in arr)
+        {
+            Console.Write(x);
+            if (x > 0 && x % 5 == 0)
+            {
+                b += x;
+            }
+        }
+        Console.Write("b:"+b);
+        
+       
+        int c = int.Parse(Console.ReadLine());
+
+       
+        
+        
+        
+    }
+}
